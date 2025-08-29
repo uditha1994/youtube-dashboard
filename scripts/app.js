a611c675870a9e3f9b289ed586f6651866b77c97
Uditha Landekumbura <uditha@programmer.net>
2025-08-29 12:06:03 +0530

    load channel recent videos

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,7 @@ const BASE_URL = 'https://www.googleapis.com/youtube/v3/';
 const searchForm = document.getElementById('searchForm');
 const channelInput = document.getElementById('channelInput');
 const channelInfo = document.getElementById('channelInfo');
-const videoGrid = document.getElementById('videosGrid');
+const videosGrid = document.getElementById('videosGrid');
 const errorMessage = document.getElementById('errorMessage');
 const loading = document.getElementById('loading');
 // const videoPlayerSection = document.getElementById('')
@@ -32,48 +32,58 @@ searchForm.addEventListener('submit', function (e) {
 });
 
 async function searchChannel(channelName) {
-    const searchUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=channel&q=${encodeURIComponent(channelName)}&key=${API_KEY}`;
+    try {
+        const searchUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=channel&q=${encodeURIComponent(channelName)}&key=${API_KEY}`;
 
-    const searchResponse = await fetch(searchUrl);
-    console.log(searchResponse);
-    const searchData = await searchResponse.json();
+        const searchResponse = await fetch(searchUrl);
+        console.log(searchResponse);
+        const searchData = await searchResponse.json();
 
-    if (!searchResponse.ok) {
-        throw new Error(searchData.error?.message || 'Failed to search for channel.');
+        if (!searchResponse.ok) {
+            throw new Error(searchData.error?.message || 'Failed to search for channel.');
+        }
+
+        if (!searchData.items || searchData.items.length === 0) {
+            throw new Error('No channel found with that name.');
+        }
+
+        // get first channel result
+        const channelId = searchData.items[0].snippet.channelId;
+        const channelTitle = searchData.items[0].snippet.channelTitle;
+        const thumbnail = searchData.items[0].snippet.thumbnails.medium.url;
+
+        //get detailed channel statistics
+        const channelUrl = `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics,contentDetails&id=${channelId}&key=${API_KEY}`;
+
+        const channelResponse = await fetch(channelUrl);
+        const channelData = await channelResponse.json();
+
+        if (!channelResponse.ok) {
+            throw new Error(channelData.error?.message ||
+                'Failed to fetch channerl details');
+        }
+
+        if (!channelData.items || channelData.items.length === 0) {
+            throw new Error('No channel details found');
+        }
+
+        const uploadsPlaylistId = channelData.items[0].contentDetails.relatedPlaylists.uploads;
+
+        //store channel data
+        currentChannelData = {
+            ...channelData.items[0],
+            thumbnail: thumbnail
+        }
+
+        //displayChannelInfo
+        displayChannelInfo(currentChannelData);
+
+        //fetch the channel'srecent videos
+        fetchVideos(uploadsPlaylistId);
+
+    } catch (error) {
+        showError(error.message || 'An error occurred while fetching channel data, plese try again...');
     }
-
-    if (!searchData.items || searchData.items.length === 0) {
-        throw new Error('No channel found with that name.');
-    }
-
-    // get first channel result
-    const channelId = searchData.items[0].snippet.channelId;
-    const channelTitle = searchData.items[0].snippet.channelTitle;
-    const thumbnail = searchData.items[0].snippet.thumbnails.medium.url;
-
-    //get detailed channel statistics
-    const channelUrl = `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics,contentDetails&id=${channelId}&key=${API_KEY}`;
-
-    const channelResponse = await fetch(channelUrl);
-    const channelData = await channelResponse.json();
-
-    if (!channelResponse.ok) {
-        throw new Error(channelData.error?.message ||
-            'Failed to fetch channerl details');
-    }
-
-    if (!channelData.items || channelData.items.length === 0) {
-        throw new Error('No channel details found');
-    }
-
-    //store channel data
-    currentChannelData = {
-        ...channelData.items[0],
-        thumbnail: thumbnail
-    }
-
-    //displayChannelInfo
-    displayChannelInfo(currentChannelData);
 }
 
 function displayChannelInfo(channel) {
@@ -108,11 +118,64 @@ function displayChannelInfo(channel) {
     channelInfo.style.display = 'grid';
 }
 
+async function fetchVideos(playlistId) {
+    try {
+        const videoUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=12&playlistId=${playlistId}&key=${API_KEY}`;
+
+        const videosResponse = await fetch(videoUrl);
+        const videosData = await videosResponse.json();
+
+        currentVideosData = videosData.items;
+
+        displayVideos(currentVideosData);
+
+    } catch (error) {
+        showError('Failed to fetch videos. Plese try again');
+        console.error(error.message, error);
+    }
+}
+function displayVideos(videos) {
+    if (!videos || videos.length === 0) {
+        videosGrid.innerHTML = '<p>No videos found for this channel</p>'
+        return;
+    }
+
+    videosGrid.innerHTML = videos.map(video => {
+        const snippet = video.snippet;
+        const videoId = snippet.resourceId.videoId;
+        const thumbnail = snippet.thumbnails.medium.url;
+        const publishedAt = new Date(snippet.publishedAt).toLocaleDateString();
+
+        return `
+            <div class="video-card" data-video-id="${videoId}" 
+            data-video-title="${snippet.title}">
+
+                <div class="video-thumbnail">
+                    <img src="${thumbnail}" alt="${snippet.title}">
+                </div>
+                <div class="video-details">
+                    <div class="video-card-title">${snippet.title}</div>
+                    <div class="video-meta">
+                        <span>Published: ${publishedAt}</span>
+                    </div>
+                </div>
+            </div>
+        `;
+    }).join('');
+}
+
+
 function clearResults() {
     channelInfo.style.display = 'none';
-    videoGrid.innerHTML = '';
+    videosGrid.innerHTML = '';
     errorMessage.style.display = 'none';
     errorMessage.textContent = '';
     currentChannelData = null;
     currentVideosData = [];
+}
+
+function showError(message) {
+    errorMessage.textContent = message;
+    errorMessage.style.display = 'block';
+    loading.style.display = 'none';
 }
